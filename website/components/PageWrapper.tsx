90145876f08348463fc657328a4062738a079875
Peter Koomen <koomen@gmail.com>
2025-03-23 10:57:09 -0700

    styling tweaks

--- website/components/PageWrapper.tsx
+++ website/components/PageWrapper.tsx
@@ -8,9 +8,9 @@ export default function PageWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen container mx-auto pt-12 px-24">
+    <div className="flex flex-col min-h-screen container mx-auto pt-12 px-24 font-sans text-gray-800 antialiased">
       <Header />
-      <div className="flex-grow py-4">
+      <div className="flex-grow py-4 leading-relaxed text-lg">
         {children}
       </div>
       <Footer />
