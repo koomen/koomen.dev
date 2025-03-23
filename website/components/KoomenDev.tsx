1ea0e4a0f7a2b4f2ccb583df928d44472b21a50e
Peter Koomen <koomen@gmail.com>
2025-03-23 13:58:14 -0700

    styling

--- website/components/KoomenDev.tsx
+++ website/components/KoomenDev.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function KoomenDev() {
   return (
-    <div className="font-mono text-xl font-semibold tracking-tight">
+    <div className="font-mono text-xl font-semibold tracking-tight text-gray-500">
       koomen.dev
     </div>
   );
