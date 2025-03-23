13f2e561dc4776d84e4c1d1c98b4a2c3a4ae1f2e
Peter Koomen <koomen@gmail.com>
2025-03-23 10:55:36 -0700

    homepage tweaks

--- website/components/PageWrapper.tsx
+++ website/components/PageWrapper.tsx
@@ -8,7 +8,7 @@ export default function PageWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen container mx-auto py-4 px-12">
+    <div className="flex flex-col min-h-screen container mx-auto pt-12 px-24">
       <Header />
       <div className="flex-grow py-4">
         {children}
