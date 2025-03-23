de7b1cffa8184977e96de0ec950a6d34c3c57093
Peter Koomen <koomen@gmail.com>
2025-03-23 11:27:17 -0700

    tweak margin top

--- website/components/PageWrapper.tsx
+++ website/components/PageWrapper.tsx
@@ -8,7 +8,7 @@ export default function PageWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen mx-auto px-4 pt-4 sm:px-8 sm:pt-8 md:px-12 md:pt-12 lg:px-24 lg:pt-16 xl:px-48 xl:pt-20 font-sans text-gray-800 antialiased">
+    <div className="flex flex-col min-h-screen mx-auto px-4 pt-4 sm:px-8 sm:pt-8 md:px-12 md:pt-12 lg:px-24 lg:pt-16 xl:px-48 xl:pt-16 font-sans text-gray-800 antialiased">
       <Header />
       <div className="flex-grow py-4 leading-relaxed text-lg">
         {children}
