703c5a819280e04a10dfc4aea0d798e9e5f2c505
Pete Koomen <koomen@gmail.com>
2025-04-20 00:41:08 -0700

    change logo

--- website/components/KoomenDev.tsx
+++ website/components/KoomenDev.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function KoomenDev() {
   return (
     <div className="font-mono text-xl tracking-tight text-gray-500">
-      <a href="/">koomen.dev</a>
+      <a href="/">Pete Koomen</a>
     </div>
   );
 }
