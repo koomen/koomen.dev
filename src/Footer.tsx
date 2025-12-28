bd5d7a9a1e03057ee6bc95ce20d0e5245884b293
Peter Koomen <koomen@gmail.com>
2025-12-27 21:59:33 -0800

    use new scratch styling

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className="py-6 mt-8 text-center text-sm text-gray-500">
       <p>
--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -1,35 +1,16 @@
 export { CodeBlock } from './CodeBlock';
-export { Code } from './Code';
-export { H1, H2, H3, H4, H5, H6, Heading } from './Heading';
-export { Image } from './Image';
+export { H2, H3 } from './Heading';
 export { default as Link } from './Link';
-export { Table } from './Table';
 
 import { CodeBlock } from './CodeBlock';
-import { Code } from './Code';
-import { H1, H2, H3, H4, H5, H6 } from './Heading';
-import { Image } from './Image';
+import { H2, H3 } from './Heading';
 import Link from './Link';
-import { Table } from './Table';
 
-// MDX component overrides
-// Only override elements that need interactivity or special handling
+// Only override elements that need interactivity
 // All other styling is handled by Tailwind Typography 'prose' class
 export const MDXComponents = {
-  // Headings with anchor links
-  h1: H1,
+  pre: CodeBlock,
   h2: H2,
   h3: H3,
-  h4: H4,
-  h5: H5,
-  h6: H6,
-  // Code with copy button
-  pre: CodeBlock,
-  code: Code,
-  // Links with external handling
   a: Link,
-  // Images with caption support
-  img: Image,
-  // Tables with overflow wrapper
-  table: Table,
 };
